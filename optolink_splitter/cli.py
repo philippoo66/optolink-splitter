f3c29308ff22489f9f206b58085420c5ec22685c
Simon Hörrle <simon.hoerrle@gmail.com>
2024-09-08 14:21:46 +0200

    add more params

--- optolink_splitter/cli.py
+++ optolink_splitter/cli.py
@@ -3,9 +3,15 @@ from optolink_splitter.optolinkvs2_switch import optolink_vs2_switch
 from typing import Optional
 
 @command()
-@option('--optolink-port', required=True, type=str, default="/dev/ttyUSB0", show_default=True, help="Usually '/dev/ttyUSB0'")
+@option('--optolink-port', required=True, type=str, default="/dev/ttyUSB0", show_default=True, help="Usually '/dev/ttyUSB0'.")
 @option('--vitoconnect-port', required=False, type=str, default=None, show_default=True, help="Usually '/dev/ttyS0'. On older Pis rather 'dev/ttyAMA0'. Does not have to be specified if no Vitoconnect is used.")
-def main(vitoconnect_port: Optional[str]) -> None:
+@option('--vitoconnect-vs2timeout', required=False, type=int, default=120, show_default=True, help="Seconds to detect VS2 protocol on vitoconnect connection.")
+@option('--mqtt-address', required=False, type=str, default=None, show_default=True, help="IP Address for mqtt Server, e.g. '192.168.0.123'. Does not have to be specified if mqtt is not used.")
+def main(optolink_port: str, 
+         vitoconnect_port: Optional[str], 
+         vitoconnect_vs2timeout: int, 
+         mqtt_address: Optional[str], 
+         ) -> None:
     print("Hello World")
     #optolink_vs2_switch()
 
