ffa849f86ec111424d8071389603ee97557db0f5
Simon Hörrle <simon.hoerrle@dm.de>
2024-09-10 09:54:28 +0200

    add two more tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,3 +1,5 @@
+import pytest
+from uuid import uuid4
 from click.testing import CliRunner
 from optolink_splitter.cli import main
 
@@ -12,7 +14,7 @@ def test_cli_smoke_test() -> None:
     )
     assert result.exit_code == 0
 
-def test_cli_poll_items_config_path_missing() -> None:
+def test_cli_option_poll_items_config_path_missing() -> None:
     runner = CliRunner()
     result = runner.invoke(
         main,
@@ -25,3 +27,18 @@ def test_cli_poll_items_config_path_missing() -> None:
     assert result.exit_code == 2
     output = result.output.strip()
     assert "Missing option '--poll-items-config-path'" in output
+
+def test_cli_poll_items_config_path_non_existent() -> None:
+    runner = CliRunner()
+    with pytest.raises(FileNotFoundError) as excinfo:
+        runner.invoke(
+            main,
+            [
+                "--optolink-port",
+                "/dev/SomePort",
+                "--poll-items-config-path",
+                f"/some/non/existent/random/path/{uuid4()}.csv"
+            ],
+            catch_exceptions=False,
+        )
+    assert "No such file or directory" in str(excinfo)
