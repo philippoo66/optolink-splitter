ad61763f3b81ca0b8a1899d1712beeb3df3f1c30
Simon Hörrle <simon.hoerrle@dm.de>
2024-09-10 09:35:59 +0200

    add test for missing required option

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,7 +1,7 @@
 from click.testing import CliRunner
 from optolink_splitter.cli import main
 
-def test_smoke_test_cli() -> None:
+def test_cli_smoke_test() -> None:
     runner = CliRunner()
     result = runner.invoke(
         main,
@@ -11,3 +11,17 @@ def test_smoke_test_cli() -> None:
         catch_exceptions=False,
     )
     assert result.exit_code == 0
+
+def test_cli_poll_items_config_path_missing() -> None:
+    runner = CliRunner()
+    result = runner.invoke(
+        main,
+        [
+            "--optolink-port",
+            "/dev/SomePort",
+        ],
+        catch_exceptions=False,
+    )
+    assert result.exit_code == 2
+    output = result.output.strip()
+    assert "Missing option '--poll-items-config-path'" in output
