689eeba08210024f9df46d087580d6281430768e
phphilippoo66 <philip.oebel@web.de>
2026-01-28 00:15:27 +0100

    V1.11.0.0 poll groups, setable

--- c_polllist.py
+++ c_polllist.py
@@ -28,6 +28,7 @@ class cPollList:
         Initialisiert die Klasse
         """
         self.items = []
+        self.cycle_groups = {}
         self.num_items = 0
         self.module_date = "0"
         # datapoint metadata cache for /set topics
@@ -48,8 +49,36 @@ class cPollList:
             if reload:
                 listmodule = importlib.reload(listmodule)
             
+            # apply poll groups (if exists)
+            if getattr(listmodule, "poll_groups", False):
+                self.cycle_groups = listmodule.poll_groups
+            # make 1-cycle
+            self.cycle_groups["*#1"] = 1
+
             # apply poll list
-            self.items = listmodule.poll_items
+            #self.items = listmodule.poll_items
+            for item in listmodule.poll_items:
+                new_item = []
+                if item[0] in self.cycle_groups:
+                    # is cycle-grouped item
+                    new_item.append(item[0])
+                    new_item.extend(item[1:])
+                elif isinstance(item[0], int):
+                    # has numeric cycle
+                    key = f"*#{item[0]}"
+                    self.cycle_groups[key] = item[0]
+                    new_item.append(key)
+                    new_item.extend(item[1:])
+                else:
+                    # no cycle given - set 1
+                    new_item.append("*#1")
+                    new_item.extend(item[0:])
+                
+                # make tuple
+                new_tuple = tuple(new_item)
+                # append to poll items
+                self.items.append(new_tuple)
+
             self.num_items = len(self.items)
             self.module_date = utils.get_module_modified_datetime(listmodule)
 
@@ -65,6 +94,17 @@ class cPollList:
             logger.error(f"make_list: {e}")
 
 
+    def set_pollcycle(self, group_key:str, value) -> bool:
+        if group_key not in self.cycle_groups:
+            return False
+        try:
+            cyc = int(value)
+        except:
+            return False
+        self.cycle_groups[group_key] = cyc
+        return True
+
+
     def find_datapoint_by_name(self, dpname):
         """Find datapoint configuration by name in poll_list."""
         if not self.items:
@@ -76,20 +116,17 @@ class cPollList:
         
         # Search in poll_list items
         for lstidx in range(self.num_items):
-            # Handle PollCycle entries: ([PollCycle,] Name, DpAddr, Len, [bbFilter,] Scale/Type, Signed)
-            item = self.items[lstidx]
-            if len(item) > 1 and isinstance(item[0], int):
-                # has PollCycle prefix
-                item = item[1:] 
+            # (PollCycleGroupKey, Name, DpAddr, Len, [bbFilter,] Scale/Type, Signed)
+            item = self.items[lstidx]   
 
-            name = item[0]
+            name = item[1]
             
-            if name == dpname:
-                addr = item[1] #if len(item1) > 1 else None
-                dlen = item[2] #if len(item1) > 2 else 1
+            if name == dpname:  #TODO cse-insesitive?!
+                addr = item[2] #if len(item1) > 1 else None
+                dlen = item[3] #if len(item1) > 2 else 1
                 #TODO bbFilter...
-                scale_type = item[3] if len(item) > 3 else None
-                signed = item[4] if len(item) > 4 else False
+                scale_type = item[4] if len(item) > 4 else None
+                signed = item[5] if len(item) > 5 else False
                 metadata = {
                     'addr': addr,
                     'len': dlen,
@@ -105,3 +142,8 @@ class cPollList:
 
 # === for global use ================
 poll_list = cPollList()
+
+# poll_list.make_list()
+# print(poll_list.num_items)
+# for itm in poll_list.items:
+#     print(itm)
